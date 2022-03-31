f506f4b3669c51425b3069cf60258ba7345154c5
Lee SeEun <sally4405@naver.com>
2022-03-31 18:39:42 +0900

    Feat: InputView xib 파일 만든 후 vc의 스택뷰에 삽입

---
+++ Signup/Signup/InputView.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class InputView: UIView {
+
+    @IBOutlet weak var inputStackView: UIStackView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit() {
+        if let view = Bundle.main.loadNibNamed("InputView", owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            addSubview(view)
+        }
+    }
+
+}
--- Signup/Signup/ViewController.swift
+++ Signup/Signup/ViewController.swift
@@ -5,6 +5,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupInputView(title: "아이디", placeHolder: "영문 소문자, 숫자, 특수기호(_,-), 5~20자", message: "이미 사용중인 아이디입니다.")
+        setupInputView(title: "비밀번호", placeHolder: "영문 대/소문자, 숫자, 특수문자(!@#$%) 8~16자", message: "안전한 비밀번호입니다.")
+        setupInputView(title: "비밀번호 재확인", placeHolder: "", message: "비밀번호가 일치하지 않습니다.")
+        setupInputView(title: "이름", placeHolder: "", message: "이름은 필수 입력 항목입니다.")
+    }
+    
+    func setupInputView(title: String, placeHolder: String, message: String) {
+        let inputView = InputView()
+        inputView.titleLabel.text = title
+        inputView.textField.placeholder = placeHolder
+        inputView.messageLabel.text = message
+        
+        stackView.addArrangedSubview(inputView)
     }
 }
 
